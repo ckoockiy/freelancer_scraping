2d387532dad655808abed7d7320f5f9b3e7cd067
cKocKiY <jorgeiranrosas@gmail.com>
2022-08-23 02:41:33 -0700

    Se extrae foto de perfil, tarifa por hora y su ubicacion

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def getFreelancer(page):
     r = requests.get(f"https://www.freelancer.mx/freelancers/{page}")
     
     if r.status_code == 200:
-        
+        print(r.url)
         with open ('data.txt', 'w', encoding="utf-8") as f:
             f.write(r.text)
 
